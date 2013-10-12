b5cfadbb6f78a8ddf291d9ec8e769acabdc323d6
Arthur Zhou <nxpzmj@126.com>
2013-10-12 14:41:23 +0800

    Add list interfaces

---
+++ ARMUE/source/utils/list.c
@@ -0,0 +1,99 @@
+#include "list.h"
+#include <stdio.h>
+
+list_t* list_create_node(list_data_t data)
+{
+    list_t *node = (list_t*)malloc(sizeof(list_t));
+    if(node == NULL){
+        goto node_null;
+    }
+    node->next = NULL;
+    node->prev = NULL;
+    node->data = data;
+    return node;
+
+node_null:
+    return NULL;
+}
+
+void list_destory_node(list_t **node)
+{
+    if(node == NULL || *node == NULL){
+        return;
+    }
+
+    free(*node);
+    *node = NULL;
+}
+
+list_t* list_create_empty()
+{
+    list_t *head = list_create_node((list_data_t)NULL);
+    if(head == NULL){
+        goto head_null;
+    }
+
+    head->prev = head;
+    head->next = head;
+
+    return head;
+
+head_null:
+    return NULL;
+}
+
+/* insert before node */
+int list_insert(list_t *node, list_t *insert)
+{
+    if(node == NULL || insert == NULL || insert->data.pdata == NULL){
+        return -1;
+    }
+
+    node->next->prev = insert;
+    insert->next = node->next;
+    node->next = insert;
+    insert->prev = node;
+}
+
+/* append after the last element */
+int list_append(list_t *head, list_t *append)
+{
+    if(append == NULL || head == NULL || append->data.pdata == NULL){
+        return -1;
+    }
+
+    while(head->data.pdata != NULL){
+        head = head->next;
+    }
+    head = head->prev;
+
+    list_insert(head, append);
+}
+
+/* insert before the first element */
+int list_ahead(list_t *head, list_t *ahead)
+{
+    if(head == NULL || ahead == NULL || ahead->data.pdata == NULL){
+        return -1;
+    }
+    while(head->data.pdata != NULL){
+        head = head->next;
+    }
+
+    list_insert(head, ahead);
+}
+
+
+list_data_t list_delete(list_t *node)
+{
+    if(node == NULL){
+        return (list_data_t)NULL;
+    }
+
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    list_data_t data = node->data;
+    list_destory_node(&node);
+
+    return data;
+}
---
+++ ARMUE/source/utils/list.h
@@ -0,0 +1,43 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+typedef union{
+    void *pdata;
+    long long idata;
+    unsigned long long udata;
+    double ddata;
+}list_data_t;
+
+typedef struct list_t{
+    list_data_t data;
+    struct list_t *next;
+    struct list_t *prev;
+}list_t;
+
+#define for_each_list_node(cur, list) \
+for((cur) = (list)->next; (cur)->data.pdata != NULL; (cur) = (cur)->next)
+
+#define list_create_node_int(data)\
+list_create_node( (list_data_t)(long long)data )
+
+#define list_create_node_ptr(data)\
+list_create_node( (list_data_t)(void*)data )
+
+#define list_create_node_double(data)\
+list_create_node( (list_data_t)(double)data )
+
+list_t*     list_create_node(list_data_t data);
+void        list_destory_node(list_t **node);
+list_t*     list_create_empty();
+int         list_insert(list_t *node, list_t *insert);
+int         list_append(list_t *head, list_t *append);
+int         list_ahead(list_t *head, list_t *ahead);
+list_data_t list_delete(list_t *node);
+
+#ifdef __cplusplus
+}
+#endif
+#endif // _LIST_H_
